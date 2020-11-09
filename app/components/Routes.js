d44a266e1309076def6958a9a5949722ee789994
Aminoor Rashid <arashid717@gmail.com>
2020-11-09 17:54:18 -0500

    initial commit

---
+++ app/components/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+const Routes = () => {
+  return (
+    <div>
+      <h1>Works!</h1>
+    </div>
+  );
+};
+
+export default Routes;
